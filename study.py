8a1bf398481495990a51f499f4f18a0803d4cea9
Jijhoon <darkmine7@inha.edu>
2024-02-02 15:30:37 +0900

    Merge sort

--- study.py
+++ study.py
@@ -1,66 +1,23 @@
+#병합정렬의 예시
+def merge_sort(a):
+    n = len(a)
+    if n <= 1:
+        return a
+    mid = n // 2
+    g1 = merge_sort(a[:mid])
+    g2 = merge_sort(a[mid:])
+    result = []
+    while g1 and g2:
+        if g1[0] < g2[0]:
+            result.append(g1.pop(0)) # pop(0)은 리스트의 맨 앞 요소를 빼내는 함수 pop을 이용해서 빼낸 수를 append로 result에 추가
+                                     # pop은 del과 다르게 삭제한 값을 반환하는 것을 이용!
+        else:
+            result.append(g2.pop(0))
+    while g1:
+        result.append(g1.pop(0))
+    while g2:
+        result.append(g2.pop(0))
+    return result
 
-def factorial(n): # 함수는 return을 만날 때 마다 반환값을 stack에 담는다.
-    number = 1
-    while n > 0:
-        number *= n
-        n -= 1
-    return number
-h = int(input("number: "))
-print(factorial(h))
-
-def reculsion(n):
-    if n == 0:
-        return 1
-    return n * reculsion(n - 1)
-print(reculsion(h)) # 내부 스택에 n * reculsion(n - 1)를 계속 담아두고 있는다.
-"""
-내부 스택의 상황은
-n * reculsion(n - 1) # n = 3                n * reculsion(n - 1) # n = 3                n * reculsion(n - 1) # n = 3    
-n * reculsion(n - 1) # n = 2        ->      n * reculsion(n - 1) # n = 2        ->      2                               
-n * reculsion(n - 1) # n = 1                1
-1
-
-final = 6
-
-선입후출을 보여준다. 먼저 stack에 쌓인 것이 나중에 처리되는 것을 알 수 있다.
-"""
-
-# def reculsion_number(n):
-#     if n == 0:
-#         return 1
-#     return n, reculsion_number(n-1)
-# print(reculsion_number(10))
-"""
-
-내부 스택
-n, reculsion_number(n-1) # 10
-n, reculsion_number(n-1) #9
-:
-n, reculsion_number(n-1) #2
-n, reculsion_number(n-1) #1
-1
-
-(2(1,1)) -> 튜플 형식으로 나온다.
-
-그렇다면 내부스택을 어떻게 만들어야 하는가?
-
-n만 나오도록 해야한다. 
-"""
-
-def reculsion_number(n):
-    if n == 0:
-        return 1
-    else:
-        reculsion_number(n-1)
-        return print(n)
-reculsion_number(10) # 1 -> 10의 순서대로 나온다. 왜냐하면 10이 먼저 스택이 쌓였기 때문이다.
-
-# 반대의 경우
-def reculsion_number_reverse(n):
-    if n == 11:
-        return 1 # 11이 되기 전까지는 계속 스택에 쌓인다.
-    else:
-        reculsion_number_reverse(n + 1)
-        return print(n)
-reculsion_number_reverse(1)
-# 10 -> 1의 순서대로 나온다. 왜냐하면 1이 먼저 스택이 쌓였기 때문이다.
+show_me = merge_sort([1, 3, 5, 72, 9, 11, 13, 12, 123, 4141, 342, 11, 9, 7, 5, 3, 1])
+print(show_me)
