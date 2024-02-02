1e0d138825871584a582304cd091f32ed8f06631
Jijhoon <darkmine7@inha.edu>
2024-02-02 17:49:43 +0900

    GCF, LCM

--- study.py
+++ study.py
@@ -1,23 +1,43 @@
-#병합정렬의 예시
-def merge_sort(a):
-    n = len(a)
-    if n <= 1:
-        return a
-    mid = n // 2 # n을 2로 나눈 몫을 mid에 저장
-    g1 = merge_sort(a[:mid]) # 재귀호출을 이용하여 반으로 나눈 리스트를 다시 merge_sort에 넣어 정렬
-    g2 = merge_sort(a[mid:]) # 재귀호출을 이용하여 반으로 나눈 리스트를 다시 merge_sort에 넣어 정렬
-    result = []
-    while g1 and g2: # 두 리스트에 요소가 남아있는 동안 반복
-        if g1[0] < g2[0]:
-            result.append(g1.pop(0)) # pop(0)은 리스트의 맨 앞 요소를 빼내는 함수 pop을 이용해서 빼낸 수를 append로 result에 추가
-                                     # pop은 del과 다르게 삭제한 값을 반환하는 것을 이용!
-        else:
-            result.append(g2.pop(0))
-    while g1:
-        result.append(g1.pop(0)) # g1에 남아있는 요소를 result에 추가
-    while g2:
-        result.append(g2.pop(0))
-    return result
+ss = "1. Something and 2. something else"
+ln = [i for i in ss if i.isdigit()][-1] # i.isdigit() returns True if i is a digit
+print(ln)
 
-show_me = merge_sort([1, 3, 5, 72, 9, 11, 13, 12, 123, 4141, 342, 11, 9, 7, 5, 3, 1])
-print(show_me)
+def filter(n):
+    flter = [i for i in n if len(i) >= 5]
+    return flter
+test = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+print(filter(test))
+
+#FizzBuzz
+def generate_number():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+generate_number()
+
+# GCF(greatest common factor) & LCM(least common multiple)
+def gcf(n, m) -> int: # n과 m의 최대공약수를 구하는 함수.
+    while m: # m이 0이 아닐 때까지 반복한다.
+        n, m = m, n % m # n에 m을, m에 n % m을 대입한다.
+    return print(n)
+gcf(100, 84)
+
+def get_lcm(a, b):
+    # 두 수 중 큰 수를 구합니다.
+    max_num = max(a, b)
+
+    while True:
+        # 큰 수부터 시작하여 공배수를 찾습니다.
+        if max_num % a == 0 and max_num % b == 0:
+            lcm = max_num
+            break
+        max_num += 1
+
+    return lcm
+print(get_lcm(3, 5))
